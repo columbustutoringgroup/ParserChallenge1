ac4fef5c8b9dde6fe7cd65138c36409397d6575c
Aaron Dawson <adawson927@gmail.com>
2016-01-07 22:42:52 -0500

    this should be significantly more concise

--- parser.rb
+++ parser.rb
@@ -1,12 +1,5 @@
 def word_in_string?(word, string)
   # implement with your code here
-	if (string.include? word)
-		if (string =~ /#{word}[a-zA-Z]+/ || string =~ /[a-zA-Z]+#{word}/)
-			return :no
-		else
-			return :yes
-		end
-	else
-		return :no
-	end
+  word_regex = /(#{word}[^a-zA-Z]+.*|.*[^a-zA-Z]+#{word}|^#{word}$)/
+  string =~ word_regex ? :yes : :no
 end
