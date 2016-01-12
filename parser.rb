995379bd9e1731b70d90387092497b13737a9dda
marcel <mkooi@covermymeds.com>
2016-01-12 09:14:56 -0500

    marcel-soln

--- parser.rb
+++ parser.rb
@@ -1,3 +1,10 @@
 def word_in_string?(word, string)
   # implement with your code here
+  if string.match(/[^A-Z^a-z]#{word}/) || string.match(/#{word}[^A-Z^a-z]/)
+  	:yes
+  else
+  	:no
+  end
+
+
 end
