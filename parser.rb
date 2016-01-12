3784920fa1cdf572eedec032e778851a905c56c6
marcel <mkooi@covermymeds.com>
2016-01-12 09:45:26 -0500

    refactored and enabled statement to be true if word and string match

--- parser.rb
+++ parser.rb
@@ -1,6 +1,6 @@
 def word_in_string?(word, string)
   # implement with your code here
-  if string.match(/[^A-Z^a-z]#{word}/) || string.match(/#{word}[^A-Z^a-z]/)
+  if string.match(/(^|[^A-Z^a-z])#{word}($|[^A-Z^a-z])/)
   	:yes
   else
   	:no
