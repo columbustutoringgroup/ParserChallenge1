ad51d173a6aa964dd3420b989ae7f969a53fbc81
Austen Madden <maddenausten@gmail.com>
2016-01-13 15:40:52 -0500

    Joe's Advice

--- parser.rb
+++ parser.rb
@@ -1,9 +1,7 @@
 def word_in_string?(word, string)
   array = string.split(/[-_\s]/)
-  array.each do |w|
-    if (w == word)
-      return :yes
-    end
+  if array.include?(word)
+    return :yes
   end
   :no
 end
