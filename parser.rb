77106753decd59c5a56d472431c463eaed32b933
matthew lee <matthewlee07@gmail.com>
2018-11-07 18:23:12 -0500

    .

--- parser.rb
+++ parser.rb
@@ -1,10 +1,5 @@
 def word_in_string?(word, string)
   # implement with your code here
-  new_string = string.downcase.gsub(/[^a-z\s]/i, " ").split(" ")
-  new_string.each { |string|
-    if word === string
-      return :yes
-    end
-  }
-  return :no
+  new_string = string.downcase.gsub(/[^a-z]/, " ").split(" ")
+  new_string.any? { |string| word == string } ? :yes : :no
 end
